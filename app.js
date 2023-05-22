24aec9599525b1079aaf1c24364797d8d9bf170b
Satish <pingsatishg@gmail.com>
2023-05-22 13:53:46 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -51,7 +49,7 @@ app.post("/register", async function (req, res) {
     }
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password:md5(req.body.password)
     })
     console.log("User registered");
     var status = await newUser.save();
@@ -62,7 +60,7 @@ app.post("/register", async function (req, res) {
 
 app.post("/login", async function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     var user = await User.findOne({ email: username })
 
